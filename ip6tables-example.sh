325734d0d7bb6075f8edcc04d1432ddca3602ad9
Chirfen <Chirfen_Z@163.com>
2020-12-10 14:09:23 +0800

    Update ip6tables-example.sh

--- ip6tables-example.sh
+++ ip6tables-example.sh
@@ -5,3 +5,4 @@ ip6tables -A PREROUTING -t mangle -p tcp --syn -d 2001:2000::/32 -j NFQUEUE --qu
 ip6tables -A PREROUTING -t nat -s 2001:1000::/32 -p tcp -j REDIRECT --to 5000
 ip6tables -A PREROUTING -t nat -d 2001:2000::/32 -p tcp -j REDIRECT --to 5000
 
+sysctl -w net.ipv6.conf.all.forwarding=1
